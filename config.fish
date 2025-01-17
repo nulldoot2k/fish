c82ea9a3327d939febc80cb307c0b3c49eca7bbd
datvm <datvm@vccloud.vn>
2025-01-17 16:17:57 +0700

    first commit

---
+++ config.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+  set -gx EDITOR nvim
+  set -gx PATH $PATH ~/.fnm ~/.yarn/bin ~/.composer/vendor/bin
+  set -gx PATH $PATH /usr/local/go-1.23.2/bin
+
+  if not set -q TMUX
+      set session_name (openssl rand -hex 2)
+      exec tmux new-session -A -s $session_name
+      exec tmux attach-session -t $session_name
+  end
+  for file in ~/.config/fish/functions/*.fish
+      source $file
+  end
+end
